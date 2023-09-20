d34d01b280facde5c401545a4c0f8fabddeb5ea0
xdJidx <bernardjerome59@gmail.com>
2023-09-20 14:27:14 +0200

    Remove error output and use request.get

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,32 +2,13 @@
 
 const request = require('request');
 const fs = require('fs');
-
-const url = process.argv[2];
+const apiURL = process.argv[2];
 const filePath = process.argv[3];
 
-if (!url || !filePath) {
-  console.error('Usage: node 5-request_store.js <URL> <FILE_PATH>');
-  process.exit(1);
-}
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+request.get(apiURL, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFileSync(filePath, body, 'utf8');
+  } else {
+    console.error(error || `Erreur: Code d'état ${response.statusCode}`);
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Status code: ${response.statusCode}`);
-    process.exit(1);
-  }
-
-  // Write the response body to the specified file (UTF-8 encoded)
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      process.exit(1);
-    }
-    console.log(`File saved as ${filePath}`);
-  });
 });
