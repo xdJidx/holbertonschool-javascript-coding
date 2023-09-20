81cc962cfb614f6702618c55db0af2ea66e891f7
xdJidx <bernardjerome59@gmail.com>
2023-09-20 09:20:32 +0200

    Remove error code

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,12 +2,6 @@
 
 const fs = require('fs');
 
-// Check if the user provided the correct number of arguments
-if (process.argv.length !== 4) {
-  console.error('Usage: node write_file.js <file_path> <string_to_write>');
-  process.exit(1);
-}
-
 // Get the file path and string to write from the command line arguments
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
@@ -17,7 +11,5 @@ fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
     // If an error occurred while writing, print the error object
     console.error(err);
-  } else {
-    console.log('File written successfully.');
   }
 });
