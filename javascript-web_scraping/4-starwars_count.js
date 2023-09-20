bc3c5217d68c61832ed52f7edc42bccff46ad4f7
xdJidx <bernardjerome59@gmail.com>
2023-09-20 09:47:06 +0200

    Add error Api, comment and change counter

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,24 +3,31 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterID = 18;
 
-request(apiUrl, function (error, response, body) {
+if (!apiUrl) {
+  console.error('Usage: node 4-starwars_count.js <API_URL>');
+  process.exit(1);
+}
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(error);
-    return;
+    console.error('Error:', error);
+    process.exit(1);
   }
 
-  const films = JSON.parse(body).results;
-  let count = 0;
-
-  for (const film of films) {
-    for (const character of film.characters) {
-      if (character.includes(`/people/${characterID}/`)) {
-        count++;
-        break; // count one time all character
-      }
-    }
+  if (response.statusCode !== 200) {
+    console.error(`Status code: ${response.statusCode}`);
+    process.exit(1);
   }
-  console.log(count);
+
+  const filmsData = JSON.parse(body);
+  const characterId = 18;
+
+  // Filter the films where Wedge Antilles (character ID 18) is present
+  const filmsAge = filmsData.results.filter((film) =>
+    film.characters.includes(`https://swapi-api.hbtn.io/api/people/${characterId}/`)
+  );
+
+  // Print the number of films where Wedge Antilles is present
+  console.log(filmsAge.length);
 });
