b70f04714ab79a9db538180d1c56b6ce488f7a6e
xdJidx <bernardjerome59@gmail.com>
2023-10-06 10:41:01 +0200

    Correction log console

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on("data", (data) => {
-  const name = data.toString().trim();
+process.stdin.on('data', () => {
+  const name = process.stdin.read();
 
   if (name) {
     console.log(`Your name is: ${name}`);
   }
 
-  console.log("This important software is now closing");
+  console.log('This important software is now closing');
   process.exit();
 });
