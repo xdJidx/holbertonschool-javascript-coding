fafadb9e4d4c7b8c8abfa42f9bd803bd7238ad04
xdJidx <bernardjerome59@gmail.com>
2023-10-06 10:43:48 +0200

    Change consol log to process

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
+  if (name !== null) process.stdout.write(`Your name is: ${name}`);
+});
 
-  if (name) {
-    console.log(`Your name is: ${name}`);
-  }
-
-  console.log('This important software is now closing');
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
