9929f34adaae2103a9d237ed09d39c961c97286c
xdJidx <bernardjerome59@gmail.com>
2023-10-09 10:03:12 +0200

    Modify body and remove error

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,11 @@
 const http = require('http');
 
-// Create an HTTP server
 const app = http.createServer((req, res) => {
-  // Set the HTTP response header to indicate plain text
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  // Send the response with the message "Hello Holberton School!"
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
 app.listen(1245);
 
-// Export the app variable
 module.exports = app;
