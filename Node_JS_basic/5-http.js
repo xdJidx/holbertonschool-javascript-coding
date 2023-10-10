40b4a4444a39b8201b93a4e24a269c652f21f7bc
xdJidx <bernardjerome59@gmail.com>
2023-10-10 09:37:43 +0200

    Correction lint : remove all double coat in single

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,34 +1,34 @@
 // Import the 'http' module to create an HTTP server
-const http = require("http");
+const http = require('http');
 // Import the 'countStudents' function
-const countStudents = require("./3-read_file_async");
+const countStudents = require('./3-read_file_async');
 
 // Create an HTTP server
 const app = http.createServer(async (req, res) => {
   // Set the status code to 200 and the content type to plain text
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  res.setHeader('Content-Type', 'text/plain');
 
   // Define route
-  if (req.url === "/") {
+  if (req.url === '/') {
     // Respond with a simple greeting
-    res.end("Hello Holberton School!");
-  } else if (req.url === "/students") {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     // Respond with a message indicating the list of students
-    res.write("This is the list of our students\n");
+    res.write('This is the list of our students\n');
 
     try {
       // Read and process student data
       const data = await countStudents(process.argv[2]);
       // Send the student data as a response
-      res.end(`${data.join("\n")}`);
+      res.end(`${data.join('\n')}`);
     } catch (error) {
       // Handle errors by sending an error message
       res.end(error.message);
     }
   } else {
     // Respond with a "Not Found" message for unrecognized routes
-    res.end("Not Found\n");
+    res.end('Not Found\n');
   }
 });
 
