59b42a818b9979c16416fcc768a586e0e5da8428
xdJidx <bernardjerome59@gmail.com>
2023-10-10 09:45:37 +0200

    Same code like 5-http.js but with express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,32 @@
+// Import the 'express' framework
 const express = require('express');
-const { countStudents } = require('./3-read_file_async');
+// Import the 'countStudents' function
+const countStudents = require('./3-read_file_async');
 
+// Create an Express application
 const app = express();
-const port = 1245;
-const databaseFile = process.argv[2] || 'database.csv';
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+// Define a route for ('/')
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 
+// Define a route for '/students'
 app.get('/students', async (req, res) => {
+  // Define a title for the response
+  const title = 'This is the list of our students\n';
+
   try {
-    const studentsInfo = await countStudents(databaseFile);
-    res.send(`This is the list of our students\n${studentsInfo}`);
+    // Read and process student data from a file specified in command-line arguments
+    const data = await countStudents(process.argv[2]);
+    // Send the student data with the title as the response
+    res.send(`${title}${data.join('\n')}`);
   } catch (error) {
-    res.status(500).send('Cannot load the database');
+    // Handle errors by sending an error message with the title as the response
+    res.send(`${title}${error.message}`);
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Start the Express application to listen on port 1245
+app.listen(1245);
 
+// Export the Express application for potential use in other modules
 module.exports = app;
